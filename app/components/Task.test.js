d00835c92116a2c226ff432acd0e617f85927c91
Miles Spoor <git@likeminded.io>
2016-09-18 02:21:39 +0100

    Added snapshot tests for Task and TaskList components

--- app/components/Task.test.js
+++ app/components/Task.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Task from './Task';
 import renderer from 'react-test-renderer';
 
-it('should include all necessary props', () => {
+it('renders the task component', () => {
   const component = renderer.create(
     <Task id={123} title="Test Task" timestamp={1474160934141} />
   );
