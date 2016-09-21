35ee3102f2117c054c2131485917c46f5152b2f9
Miles Spoor <git@likeminded.io>
2016-09-21 21:26:27 +0100

    Added two more action tests Changed navigation so it lints better

--- app/actions/index.test.js
+++ app/actions/index.test.js
@@ -2,7 +2,7 @@ import * as types from './types';
 import * as actions from './';
 
 describe('actions', () => {
-  it('it should create an action to change the filter type', () => {
+  it('should create an action to change the filter type', () => {
     const args = ['TEST'];
     const result = {
       type: types.CHANGE_FILTER_TYPE,
@@ -12,7 +12,7 @@ describe('actions', () => {
     expect(actions.changeFilterType.apply(null, args)).toEqual(result);
   });
 
-  it('it should create an action to add a task', () => {
+  it('should create an action to add a task', () => {
     const args = [{
       id: 1,
     }];
@@ -24,5 +24,28 @@ describe('actions', () => {
     };
 
     expect(actions.addTask.apply(null, args)).toEqual(result);
+  });
+
+  it('should create an action to change a task type', () => {
+    const args = [1, 'TEST'];
+    const result = {
+      type: types.CHANGE_TASK_TYPE,
+      task: {
+        id: 1,
+        type: 'TEST',
+      },
+    };
+
+    expect(actions.changeTaskType.apply(null, args)).toEqual(result);
+  });
+
+  it('should create an action to remove a task', () => {
+    const args = [1];
+    const result = {
+      type: types.REMOVE_TASK,
+      id: 1,
+    };
+
+    expect(actions.removeTask.apply(null, args)).toEqual(result);
   });
 });
